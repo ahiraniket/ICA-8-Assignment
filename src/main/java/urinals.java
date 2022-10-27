7b401fa935b77721c79b789e15ebf692e2b0801f
Aniket Ahir <ahiraniket@outlook.in>
2022-10-27 13:48:27 -0700

    goodString added

---
+++ src/main/java/urinals.java
@@ -0,0 +1,10 @@
+public class urinals {
+    public static void main(String[] args) {
+
+    }
+    public Boolean goodString(String str)
+    {
+        System.out.println("Not yet implemented!");
+        return true;
+    }
+}
