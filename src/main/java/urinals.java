a1291fc9dcbaf4e4d8703327c05aebe364546ef4
Aniket Ahir <ahiraniket@outlook.in>
2022-10-27 14:05:26 -0700

    user keyboard input recorded

--- src/main/java/urinals.java
+++ src/main/java/urinals.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class urinals {
     public static void main(String[] args) {
-        goodString("some random input");
+        String input = new String();
+        Scanner in = new Scanner(System.in);
+        System.out.print("\nEnter Input:");
+        input = in.nextLine();
+        goodString(input);
     }
-    public static Boolean goodString(String str)
-    {
+
+    public static Boolean goodString(String str) {
+        System.out.println("Input = " + str);
         System.out.println("Not yet implemented!");
         return true;
     }
